59a1d5c344aea98e37fe442b5ee8cde9b113a9d1
Darwin Gualotuña <daosgc@gmail.com>
2024-08-01 08:37:28 -0500

    Update apolo API and sandbox page

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,12 +1,13 @@
-import { getBackendData } from "../_services/api";
+import { getWebsiteData } from "../_services/api";
 
 export default async function SandBox() {
-  const data = await getBackendData();
+  const data = await getWebsiteData();
+  const logoUrl = data.layout[0].logoImage.url;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>{data?.name}</h2>
-      <p>{data.description}</p>
+      <h2>{data?.WebsiteName}</h2>
+      <img src={`${process.env.APOLO_API_URL}${logoUrl}`} alt="img" />
     </main>
   );
 }
