26d8ee63fc01d11d18efb3235e593d208860b12c
Ricardo Capa <ricardocmdn@gmail.com>
2024-08-02 22:12:07 -0500

    add page configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,65 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // fontFamily: {
+      //   'inter': ['Inter', 'sans-serif'],
+      // },
+      // fontSize: {
+      //   'body-apolo': ['13px', '15.21px'],
+      // },
+      // fontWeight: {
+      //   'body-apolo': '500',
+      // },
+      colors: {
+        primary: {
+          100: '#ffd5eb',
+          200: '#ffafda',
+          300: '#ff8ac8',
+          400: '#ff64b7',
+          500: '#ff3ea5',
+          600: '#ff1993',
+          DEFAULT: '#F20081',
+          800: '#ca026d',
+        },
+        secondary: {
+          100: '#82a7e2',
+          200: '#6591d9',
+          300: '#497cd0',
+          400: '#3469be',
+          500: '#2e599f',
+          600: '#274980',
+          700: '#1f3962',
+          800: '#172945',
+          DEFAULT: '#0e1828',
+        },
+        black: {
+          DEFAULT: '#000000',
+          light: '#e3e4eb',
+          footer: '#262626',
+          'dark-light': 'rgba(14,23,38,.15)',
+        },
+        white: {
+          DEFAULT: '#ffffff',
+          beige: '#F4F4F2',
+          light: '#b9bdc4',
+          dark: '#888ea8',
+        },
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            'h1': {
+              fontSize: '58px',
+              fontWeight: '900',
+              lineHeight: '49.3px',
+            },
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 };
 export default config;
