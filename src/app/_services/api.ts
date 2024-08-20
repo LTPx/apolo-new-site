c9f4da9c4018df71864c403b992924e2614324b4
Lenin Capa <leviskp87@gmail.com>
2024-08-19 20:35:38 -0500

    no cache

--- src/app/_services/api.ts
+++ src/app/_services/api.ts
@@ -3,7 +3,11 @@ import { ApoloLayout } from "../_interfaces/layout";
 export async function getWebsiteData(): Promise<ApoloLayout> {
     const url = `${process.env.APOLO_API_URL}/api/websites/662a93053afb198b0683ca2f?locale=fr`;
     console.log('url: ', url)
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      next: {
+        revalidate: 0,
+      },
+    });
     const dataJson = await response.json();
     if (!response.ok) throw new Error(dataJson.message);
     return dataJson;
